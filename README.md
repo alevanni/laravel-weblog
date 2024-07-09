af0165c9b1820d77392f5bc022b321ad0b9633e7
Alessia Vanni <alevanni19@gmail.com>
2024-07-09 16:02:39 +0200

    I created the project
    - I set up laravel
    - I created the database relationships diagram
    - I created the models
    - I created most of the database relationships in the models
    - I drafted some very bad flowchart (with pen and paper)
    - I have to do the migrations

---
+++ README.md
@@ -0,0 +1,5 @@
+## For self:
+- Finish creating models and migrations
+- Here is the database structure: https://dbdiagram.io/d/weblog-668cfc649939893dae6d6019
+- Here you can draft a flowchart: https://app.diagrams.net/
+- Here is a guide about pivot tables: https://laraveldaily.com/post/pivot-tables-and-many-to-many-relationships 
---
+++ app/Models/Article.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Article extends Model
+{
+    use HasFactory;
+    protected $fillable = ['title', 'body', 'user_id', 'image', 'premium'];
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+    public function comments() {
+        return $this->hasMany(Comment::class);
+    }
+    public function categories() {
+        return $this->belongsToMany(Category::class);
+    }
+}
